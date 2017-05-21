2cd9374199780b83f62b8a49fc3fe934029c8a36
Harry Vu <harryvu801@gmail.com>
2017-05-21 01:20:32 -0600

    home views almost done

--- public/sheetsView/js/sheetsCtrl.js
+++ public/sheetsView/js/sheetsCtrl.js
@@ -1,4 +1,5 @@
-app.controller('sheetsCtrl', function ($scope, $state) {
+app.controller('sheetsCtrl', function ($scope, $state, sheetsSrvc) {
   $scope.broken = "sheets Home view"
   $scope.alsoBroken = "You are now working in a new SHEET"
+  $scope.templatePics = sheetsSrvc.sheetsTemplates;
 })
