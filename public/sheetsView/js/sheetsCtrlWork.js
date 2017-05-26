a53ae9be1720a59898ef286632aa7c10658e0ef4
Andrew Chen <ac12491@gmail.com>
2017-05-26 11:06:44 -0600

    slideout menu update

--- public/sheetsView/js/sheetsCtrlWork.js
+++ public/sheetsView/js/sheetsCtrlWork.js
@@ -4,45 +4,61 @@ GoogleApps.controller('sheetsCtrlWork', function($scope, $state, sheetsSrvc) {
   $scope.templatePics = sheetsSrvc.sheetsTemplates;
   $scope.test = 'test';
 
-  $scope.inputcells = function(){
-		var array = new Array(30);
-		var array2 = new Array(13);
-		var array3 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P'];
-		$scope.cells = [];
-		$scope.cellsHOR = [];
-		$scope.abc = [];
-		for (var i = 1; i < array.length; i++) {
-			$scope.cells.push(i)
-		}
-		for (var i = 0; i < array2.length; i++) {
-			$scope.cellsHOR.push(i)
-      $scope.abc.push(array3[i])
-		}
-    return $scope.cells, $scope.cellsHOR, $scope.abc
-	}
-	$scope.inputcells();
-
-  $scope.calc = function(para){
-    if(para === 'pi'){
-
+  $scope.inputcells = function() {
+    var array = new Array(30);
+    var array2 = new Array(13);
+    var array3 = [
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+      'G',
+      'H',
+      'I',
+      'J',
+      'K',
+      'L',
+      'M',
+      'N',
+      'O',
+      'P'
+    ];
+    $scope.cells = [];
+    $scope.cellsHOR = [];
+    $scope.abc = [];
+    for (var i = 1; i < array.length; i++) {
+      $scope.cells.push(i)
     }
+    for (var i = 0; i < array2.length; i++) {
+      $scope.cellsHOR.push(i)
+      $scope.abc.push(array3[i])
+    }
+    return $scope.cells,
+    $scope.cellsHOR,
+    $scope.abc
+  }
+  $scope.inputcells();
+
+  $scope.calc = function(para) {
+    if (para === 'pi') {}
 
     console.log($scope.cellvalue);
     return $scope.cellvalue
   }
 
-
-  $scope.fun = function(para){
-    if(para==='on'){
-        this.select();
-      }
-    else{
-       this.unselect();
+  $scope.fun = function(para) {
+    if (para === 'on') {
+      this.select();
+    } else {
+      this.unselect();
     }
   }
 
-
-
-
+  
+  $(function() {
+    $("#selectable").selectable();
+  });
 
 })
